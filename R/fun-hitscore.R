8bfcb3c3027e34c96892c4d409d4dc2b378ea435
Aleksander Chlebowski <olobiolo@gmail.com>
2021-10-05 15:47:45 +0200

    update function hitscore

--- R/fun-hitscore.R
+++ R/fun-hitscore.R
@@ -1,32 +1,39 @@
-#' assign hit scoes
+#' assign hit scores
 #'
 #' Assign hit status to single wells in a screen based on their z scores.
 #'
 #' A data point is considered a hit if its hitting variable (usually a z score)
-#' exceeds a treshold. A point can have one of three hitscores:
+#' exceeds a threshold. A point can have one of three hitscores:
 #' \itemize{
 #'   \item{
-#'   1 (positive hit) if its value is equal to or higher than the treshold
+#'   1 (positive hit) if its value is equal to or higher than the upper cut-off threshold
 #'   }
 #'   \item{
-#'   -1 (negative hit) if its value is equal or lowerto  than the negative treshold
+#'   -1 (negative hit) if its value is equal to or lower than the lower cut-off threshold
 #'   }
 #'   \item{
-#'   0 (non-hit) if its absolute value is lower than the treshold
+#'   0 (non-hit) otherwise
 #'   }
 #' }
 #'
 #' @param x a numeric vector
-#' @param treshold absolute treshold for hit status
+#' @param threshold numeric vector specifying the lower- and upper cut-off threshold;
+#'                  normally of length 2, but a symmetric (e.g. -3 and 3) threshold
+#'                  can be specified with a single numeric;
 #'
 #' @return a vector of hit statuses represented as integers: 1, -1 or 0
 #'
 #' @export
 
-hitscore <- function(x, treshold = 2) {
+hitscore <- function(x, threshold = 2) {
   if (!is.numeric(x)) stop('"x" must be numeric')
-  x1 <- ifelse(abs(x) < treshold, 0L, x)
-  x2 <- ifelse(x1 <= -treshold, -1L, x1)
-  x3 <- ifelse(x2 >= treshold, 1L, x2)
+  if (length(threshold) > 2L) stop("\"threshold\" must be of length 1 or 2")
+
+  if (length(threshold) == 1L) {
+    threshold <- c(ithreshold, threshold)
+  }
+  x1 <- ifelse(x > threshold[1] & x < threshold[2], 0L, x)
+  x2 <- ifelse(x1 <= threshold[1], -1L, x1)
+  x3 <- ifelse(x2 >= threshold[2], 1L, x2)
   return(x3)
 }
